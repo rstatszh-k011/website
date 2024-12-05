724fcb7aeb608c5b0d50a068cfd078ec495b4877
Lars Schöbitz <lschoebitz@ethz.ch>
2024-12-05 14:06:10 +0200

    Modulseite überarbeiten

--- data/importiere_kursdaten.R
+++ data/importiere_kursdaten.R
@@ -14,25 +14,25 @@ library(dplyr)
 
 ## course schedule  --------------------------------------------------
 
-kursplan <- "https://docs.google.com/spreadsheets/d/1zNG1qrFBDH8l76G-DQNaHDejbS3PR75_zmK0_7LiARY/edit?gid=0#gid=0"
+kursplan <- "https://docs.google.com/spreadsheets/d/1NwrpVQRRxSSsnUaKVe-D089WJL7XKCZcQH2Ef1-GOYc/edit?gid=0#gid=0"
 
 read_sheet(ss = kursplan) |> 
     mutate(titel = case_when(
         is.na(link) == FALSE ~  paste0("[", titel, "](", link, ")"),
         TRUE ~ titel
     )) |>
-    write_csv(here::here("data/tbl-00-rstatszh-k009-kursplan.csv"))
+    write_csv(here::here("data/tbl-00-rstatszh-kursplan.csv"))
 
 ## learning objectives  ------------------------------
 
 lernziele <- "https://docs.google.com/spreadsheets/d/1Psu_bKUJ0evgDUorkoeV3KUGNDdSte--6GBYuuihQc0/edit?gid=0#gid=0"
 
 read_sheet(ss = lernziele) |> 
-    write_csv(here::here("data/tbl-01-rstatszh-k009-lernziele.csv"))
+    write_csv(here::here("data/tbl-01-rstatszh-lernziele.csv"))
 
 ## abschlussprojekt
 
 abschlussprojekt <- "https://docs.google.com/spreadsheets/d/1kyPzhG_0oZ7njXaKh6uIIW0yICEHySSEShlfPmzxviM/edit?gid=0#gid=0"
 
 read_sheet(ss = abschlussprojekt) |> 
-    write_csv(here::here("data/tbl-02-rstatszh-k009-abschlussprojekt.csv"))
+    write_csv(here::here("data/tbl-02-rstatszh-abschlussprojekt.csv"))
